95afa908d230a55c200342cead344a2277768c2b
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-28 21:48:06 +0530

    implement sliderItem

--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -1,19 +1,20 @@
-import {Dimensions, Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import { NewsDataType } from "@/types";
-import Animated, { Extrapolation, interpolate, SharedValue, useAnimatedStyle } from "react-native-reanimated";
-import { LinearGradient } from "expo-linear-gradient";
-import { Colors } from "@/constants/Colors";
-import {Link} from "expo-router"
+import {Image, Dimensions, StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import {NewsDataType} from "@/types";
+import Animated, {Extrapolation, interpolate, SharedValue, useAnimatedStyle} from "react-native-reanimated";
+import {LinearGradient} from "expo-linear-gradient";
+import {Colors} from "@/constants/Colors";
+import {Link} from "expo-router";
+
 
 type Props = {
-    sliderItem: NewsDataType;
-    index: number;
-    scrollx: SharedValue<number>;
-};
+    sliderItem: NewsDataType
+    index: number
+    scrollx : SharedValue<number>
+}
 
-const { width } = Dimensions.get("screen");
+const {width} = Dimensions.get("screen");
 
-const SliderItem = ({ sliderItem, index, scrollx }: Props) => {
+const SliderItem = ({sliderItem,index,scrollx}: Props) => {
     const rnStyle = useAnimatedStyle(() => {
         return {
             transform: [
@@ -39,72 +40,72 @@ const SliderItem = ({ sliderItem, index, scrollx }: Props) => {
 
     return (
         <Link href={`/news/${sliderItem.article_id}`} asChild>
-        <TouchableOpacity>
-        <Animated.View style={[styles.itemWrapper, rnStyle]}>
-            <Image source={{ uri: sliderItem.image_url }} style={styles.image} />
-            <LinearGradient colors={["transparent", "rgba(0,0,0,0.8)"]} style={styles.background}>
-                <View style={styles.sourceInfo}>
-                    {sliderItem.source_icon && (
-                        <Image source={{ uri: sliderItem.source_icon }} style={styles.sourceIcon} />
-                    )}
-                    <Text style={styles.sourceName}>{sliderItem.source_name}</Text>
-                </View>
-                <Text style={styles.title} numberOfLines={2}>
-                    {sliderItem.title}
-                </Text>
-            </LinearGradient>
-        </Animated.View>
-        </TouchableOpacity>
-       </Link>
-    );
-};
+            <TouchableOpacity>
+                <Animated.View style={[styles.itemWrapper,rnStyle]}>
+                    <Image source={{uri:sliderItem.image_url}} style={styles.image}/>
+                    <LinearGradient  colors={["transparent",'rgba(0,0,0,0.8)']} style={styles.background}>
+                        <View style={styles.sourceInfo}>
+                            {sliderItem.source_icon && (
+                                <Image source={{uri:sliderItem.source_icon}} style={styles.sourceIcon} />
+                            )}
+                            <Text style={styles.sourceName}>{sliderItem.source_name}</Text>
+                        </View>
+                        <Text style={styles.title} numberOfLines={2}>{sliderItem.title}</Text>
+                    </LinearGradient>
+                </Animated.View>
+            </TouchableOpacity>
+        </Link>
+    )
+}
 
-export default SliderItem;
+export default SliderItem
 
 const styles = StyleSheet.create({
     itemWrapper: {
-        width: width,
-        justifyContent: "center",
-        alignItems: "center",
+        // position:'relative',
+        width:width,
+        justifyContent:'center',
+        alignItems:'center'
     },
     image: {
-        width: width - 50,
-        height: 180,
-        borderRadius: 20,
+        width:width - 60,
+        height:180,
+        borderRadius:20,
     },
     background: {
-        position: "absolute",
-        left: 28,
-        top: 0,
-        width: width - 50,
-        height: 180,
-        borderRadius: 20,
-        padding: 33,
+        position:"absolute",
+        left:30,
+        top:0,
+        width:width - 60,
+        height:180,
+        borderRadius:20,
+        padding:20
     },
     sourceIcon: {
-        width: 25,
-        height: 25,
-        borderRadius: 20,
+        width:25,
+        height:25,
+        borderRadius:20
     },
     sourceInfo: {
-        flexDirection: "row",
-        position: "absolute",
-        top: 85,
-        paddingHorizontal: 20,
-        alignItems: "center",
-        gap: 10,
+        flexDirection:'row',
+        position:"absolute",
+        top:85,
+        paddingHorizontal:20,
+        alignItems:'center',
+        gap:10
     },
     sourceName: {
-        color: Colors.white,
-        fontSize: 12,
-        fontWeight: "600",
+        color:Colors.white,
+        fontSize:12,
+        fontWeight:'600'
     },
-    title: {
-        fontSize: 14,
-        color: Colors.white,
-        position: "absolute",
-        top: 120,
-        paddingHorizontal: 20,
-        fontWeight: "600",
-    },
-});
+    title:{
+        fontSize:14,
+        color:Colors.white,
+        position:'absolute',
+        top:120,
+        paddingHorizontal:20,
+        fontWeight:'600'
+    }
+
+})
